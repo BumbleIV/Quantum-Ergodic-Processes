12519e5af2ab33165f3d8b85c926d5e234d6746e
bumbleIV <linganda@msu.edu>
2022-01-25 02:41:15 -0500

    upload MATLAB files

---
+++ old/MATLAB/RBloch.m
@@ -0,0 +1,45 @@
+lambda=.05;
+delta = pi/12;
+N=5000;
+v0 = [1 0 0]';
+theta = pi/2;
+p=.4;
+%marr=50;
+
+
+v = zeros(3,N+1);
+v(:,1)=v0;
+
+for j=1:N
+    phi = 2*pi*rand;
+    u = [sin(theta)*cos(phi), sin(theta)*sin(phi),cos(theta)]';
+    v(:,j+1) = lambda * u + (1-lambda)*dot(u,v(:,j))*u... 
+        +sqrt(1-lambda)*(cos(delta)*(v(:,j)- dot(u,v(:,j))*u)...
+            + sin(delta)*cross(v(:,j),u));
+end
+
+figure;
+[x y z] = sphere;
+ax1 = axes;
+h = surfl(ax1,x,y,z);
+set(h,'EdgeAlpha',0.1,'FaceAlpha',.1);
+colormap(ax1,summer);
+axis equal;
+ax2 = axes;
+norm = vecnorm(v,2,1);
+%colors = 0.75*[(1-norm(:)).^p (1-norm(:)).^p (1-norm(:)).^p];
+%colors = [0.75+0.25*norm(:).^p.*(1-norm(:)).^p  0.75*(1-norm(:)).^p 0.75*(1-norm(:)).^p];
+colors = [0.3*(1-norm(:)).^p 0.6*(1-norm(:)).^p 0.9*(1-norm(:)).^p];
+%colors = [0.1+0.9*norm(:).^p.*(1-norm(:)).^p  0.1 + 0.9*norm(:).^p.*(1-norm(:)).^p (1-norm(:)).^p];
+%colors = [zeros(size(norm(:))) (1-norm(:)).^p  0.5 + 0.5*norm(:).^p];
+%colors = [zeros(size(norm(:))) norm(:).^p 0.5 + 0.5*(1-norm(:)).^p ];
+%colors = [(norm(:)).^p 0*(1-(norm(:)).^p) (1-(norm(:)).^p)];
+scatter3(ax2,v(1,:),v(2,:),v(3,:),20,colors,'filled');
+hold on;
+scatter3(ax2,v(1,1),v(2,1),v(3,1),20,'r+');
+patch(ax2,[v(1,:) nan] ,[v(2,:) nan],[v(3,:) nan],1:(N+2),'EdgeColor','interp','FaceColor','none');
+colormap(ax2,colors);
+axis equal;
+hLink = linkprop([ax1,ax2],{'XLim','YLim','ZLim','CameraUpVector','CameraPosition','CameraTarget'});
+ax1.Visible = 'off';
+ax2.Visible = 'off';
---
+++ old/MATLAB/TIBloch.m
@@ -0,0 +1,50 @@
+lambda=.1;
+delta = pi/12;
+N=2000;
+v0 = [1 0 0]';
+theta = pi/2;
+m = 3;
+omega = 2*pi/m;
+p=0.4;
+
+phi = 0;
+v = zeros(3,N+1);
+v(:,1)=v0;
+
+for j=1:N
+    w = v(:,j);
+    for k=1:m
+        u = [sin(theta)*cos(phi), sin(theta)*sin(phi),cos(theta)]';
+        phi = phi + omega;
+        w = lambda * u + (1-lambda)*dot(u,w)*u... 
+            +sqrt(1-lambda)*(cos(delta)*(w- dot(u,w)*u)...
+            + sin(delta)*cross(w,u));
+    end
+    v(:,j+1) = w;
+end
+
+figure;
+[x y z] = sphere;
+ax1 = axes;
+h = surfl(ax1,x,y,z);
+set(h,'EdgeAlpha',0.1,'FaceAlpha',.1);
+colormap(ax1,summer);
+axis equal;
+ax2 = axes;
+norm = vecnorm(v,2,1);
+%colors = 0.75*[(1-norm(:)).^p (1-norm(:)).^p (1-norm(:)).^p];
+%colors = [0.75+0.25*norm(:).^p.*(1-norm(:)).^p  0.75*(1-norm(:)).^p 0.75*(1-norm(:)).^p];
+colors = [0.3*(1-norm(:)).^p 0.6*(1-norm(:)).^p 0.9*(1-norm(:)).^p];
+%colors = [0.1+0.9*norm(:).^p.*(1-norm(:)).^p  0.1 + 0.9*norm(:).^p.*(1-norm(:)).^p (1-norm(:)).^p];
+%colors = [zeros(size(norm(:))) (1-norm(:)).^p  0.5 + 0.5*norm(:).^p];
+%colors = [zeros(size(norm(:))) norm(:).^p 0.5 + 0.5*(1-norm(:)).^p ];
+%colors = [(norm(:)).^p 0*(1-(norm(:)).^p) (1-(norm(:)).^p)];
+scatter3(ax2,v(1,:),v(2,:),v(3,:),20,colors,'filled');
+hold on;
+scatter3(ax2,v(1,1),v(2,1),v(3,1),20,'r+');
+patch(ax2,[v(1,:) nan] ,[v(2,:) nan],[v(3,:) nan],1:(N+2),'EdgeColor','interp','FaceColor','none');
+colormap(ax2,colors);
+axis equal;
+hLink = linkprop([ax1,ax2],{'XLim','YLim','ZLim','CameraUpVector','CameraPosition','CameraTarget'});
+ax1.Visible = 'off';
+ax2.Visible = 'off';
